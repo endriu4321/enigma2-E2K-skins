8105ed8aff675eadd988705a1f77ad7a33b23225
azman26 <40336898+azman26@users.noreply.github.com>
2025-05-31 22:31:40 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -32,6 +32,7 @@ fi
 echo "Kopiowanie katalogów do $DEST_DIR..."
 cp -r enigma2-E2K-skins-main/azman-E2K-skins "$DEST_DIR/"
 cp -r enigma2-E2K-skins-main/endriu-E2K-skins "$DEST_DIR/"
+cp -r enigma2-E2K-skins-main/endriu-V2-E2K-skins "$DEST_DIR/"
 cp -r enigma2-E2K-skins-main/endriu-Blu-Line-E2K-skins "$DEST_DIR/"
 cp -r enigma2-E2K-skins-main/jk36-E2K-skins "$DEST_DIR/"
 
